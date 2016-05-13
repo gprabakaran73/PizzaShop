dc07b232978f0ed64ddc9b32c21ba5da02e039b0
Prabkaaran Gopal <gprabakaran73@gmail.com>
2016-05-13 14:20:53 +1000

    Initial Drop

---
+++ gruntfile.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Shawn on 10/15/2014.
+ */
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      all: ["./public/js/**/*.js"],
+      options: {
+        unused: true
+      }
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-contrib-jshint");
+
+  grunt.registerInitTask("default", ["jshint"]);
+};
